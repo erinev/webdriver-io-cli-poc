af14de614c8fd6554e4397e6af2e1b6a657cc541
Erikas Neverdauskas <e.neverdauskas@adform.com>
2019-01-27 21:35:38 +0200

    WIP get element helpers

--- utils/page-util.js
+++ utils/page-util.js
@@ -9,4 +9,28 @@ module.exports = class PageUtil {
         
         element.waitForDisplayed(browser.config.waitforTimeout, reverseCondition);
     };
+
+    static getExistingElement(selector, element) {
+        let returnedElement;
+        if (element) {
+            browser.waitUntil(
+                () => element.$(selector).isExisting() && element.$(selector).value, 
+                null, 
+                `$(${element.selector}).$(${selector}) does not exist`);
+            returnedElement = element.$(selector);
+        } else {
+            browser.waitUntil(
+                () => $(selector).isExisting() && $(selector).value, 
+                null, 
+                `$(${selector}) does not exist`);
+            returnedElement = $(selector);
+        }
+        return returnedElement;
+    };
+    
+    static getExistingVisibleElement(selector, elem) {
+        const el = getExistingElement(selector, elem);
+        el.waitForVisible();
+        return el;
+    };
 }
